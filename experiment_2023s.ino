d9ea5686efe1033c79841272128e522dc9de0719
skrbcr <90087433+skrbcr@users.noreply.github.com>
2023-07-04 14:00:22 +0900

    Update experiment_2023s.ino

--- experiment_2023s.ino
+++ experiment_2023s.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+// 信号電圧をワットに変換
+// vol: 信号電圧[mV]
+// hertz: 周波数[MHz]
+double vol_to_watt(double vol, double hertz);
 
+void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+}
 
+double vol_to_watt(double vol, double hertz) {
 }
